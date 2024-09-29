c7d1fe5ce4b67006a862780e93ea75893439be1e
JuSeRDev <juserdev@gmail.com>
2024-09-28 23:05:49 -0500

    Codigo JS Terminado

--- ping-coming-soon-page-master/code.js
+++ ping-coming-soon-page-master/code.js
@@ -1,2 +1,26 @@
-button = document.querySelector(".button")
+const button = document.querySelector(".button")
+const error = document.querySelector(".error")
+const input = document.querySelector(".input-text")
+
+
+
+const validate = ()=>{
+    if (input.value.indexOf("@") === -1 || input.value.indexOf(".") === -1 ) {
+        error.style.display = "flex"
+        console.log("codigo no enviado");
+        
+    } else{
+        error.style.display = "none"
+        console.log("codigo enviado");
+        input.value = ""
+    }
+}
+
+button.addEventListener("click", ()=>{
+    let error = validate()
+    if (!error) {
+        return false
+    }
+})
+
 
